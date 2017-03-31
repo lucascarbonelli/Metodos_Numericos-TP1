154d66379a0fa1f1710e314e9784bcf7ffff05f6
istng <ivanstng@mail.com>
2017-03-31 20:43:27 -0300

    EL PARSEADOR SIN COMPILAR

--- parseador.cpp
+++ parseador.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-
+//mapeo la fila de el dat como un array de strings: fecha ganador gPuntos perdedor pPuntos
 std::string[] rowToArray(string line){
 	std::string[5] datos;
 	int i = 0;
@@ -14,17 +14,28 @@ std::string[] rowToArray(string line){
 	return datos;
 }
 
+int i busquedaIndice(std::string datos, std::vector<int> ids) {
+	int i = 0;
+	while(i < ids.size()) {
+		if (datos == ids[i]){
+			break;
+		}
+		i++;
+	}
+	return i;
+}
+
 std::vector<vector<int> > parseadorParaTenis(){
-	fstream partidos("LOSPARTIDOSDETENIS");
+	fstream partidos("LOSPARTIDOSDETENIS"); //abro el .dat de los partidos de tenis TODO
 	std::string line;
-	int lines = 1;
+	int lines = 1; //lines empieza con uno pero flechita hacia abajo
 	std::vector<int> ids = ids();
 
 	//hago la primera iteracion a manopla para no tener el vector de ids vacio
 	getline(partidos,line);
 	string[5] datos = rowToArray(line);
-	id.push_back(datos[1]);
-	id.push_back(datos[3]);
+	ids.push_back(datos[1]);
+	ids.push_back(datos[3]);
 	
 	// consigo las ids
 	for(lines;std::getline(partidos,line);lines++){
@@ -55,10 +66,19 @@ std::vector<vector<int> > parseadorParaTenis(){
 		}
 	}
 	partidos.clear();
-	partidos.sekkg(0,ios::beg);
-	std::vector<vector<int> > victorias = victorias(lines,vector<int>(3));
-	std::vector<vector<int> > enfrentamientos = enfrentamientos(lines,vector<int>lines);
- 	//TODO: llenar las victorias y enfrentamientos
-	//while()
+	partidos.seekg(0,ios::beg);
+	std::vector<vector<int> > victDerrt = victDerrt(ids.size(),vector<int>(2, 0));
+	std::vector<vector<int> > enfrentamientos = enfrentamientos(ids.size(),vector<int>(ids.size()), 0);
 
+	lines = 0;
+	for (lines;std::getline(partidos,line);lines++) {
+		string[5] datos = rowToArray(line);
+		
+		int i = busquedaIndice(datos[1], ids);
+		int j = busquedaIndice(datos[3], ids);
+
+		victDerrt[i][0]++; //victorias
+		victDerrt[j][1]++; //derrotas
+		enfrentamientos[i][j]++;
+	}
 }
