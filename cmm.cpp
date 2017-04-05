12733d837fc6a43c60fd511710dc30f155d9b2d4
pato <patricio.saboga@dinmax.com>
2017-04-05 10:38:30 -0300

    ahora anda mejor, pero no esta del todo bien

--- cmm.cpp
+++ cmm.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 //Toma los datos parseados como la matriz A talque A[i][j] representa la cantidad de partidos jugados entre i y j
 vector<vector<int> > CMM(vector<vector<int> > datos){
- int tam = datos.size();
- vector<vector<int> > cmm(tam,vector<int>(tam));
- int i = 0;
- int j = 0;
- while(i < tam ){
-  j = 0;
-  while(j < tam){
-   if (i == j) {
-    cmm[i][j] = 2 + datos[i][j];
-   }
-   else{
-    cmm[i][j] = -datos[i][j];
-   }
-   j++;
-  }
-  i++;
- }
- return cmm;
+	int tam = datos.size();
+	vector<vector<int> > cmm(tam,vector<int>(tam,0));
+	int i = 0;
+	int j = 0;
+	while(i < tam ){
+		j = 0;
+		while(j < tam){
+			if (i == j) {
+				cmm[i][j] = 2 + datos[i][j];
+			}
+			else{
+				cmm[i][j] = -(datos[i][j]);
+			}
+			j++;
+		}
+		i++;
+	}	
+	return cmm;
 }
 
 
--- parseador.cpp
+++ parseador.cpp
@@ -29,64 +29,7 @@ int busquedaIndice(string datos, vector<string> ids) {
 	return i;
 }
 
-vector<vector<int> > parseadorParaTenis(){
-	fstream partidos("atp_matches_2015.dat"); //abro el .dat de los partidos de tenis TODO
-	string line;
-	int lines = 1; //lines empieza con uno pero flechita hacia abajo
-	vector<string> ids;
 
-	//hago la primera iteracion a manopla para no tener el vector de ids vacio
-	getline(partidos,line);
-	vector<string> datos = rowToArray(line);
-	ids.push_back(datos[1]);
-	ids.push_back(datos[3]);
-	
-	// consigo las ids
-	for(;getline(partidos,line);lines++){
-		vector<string> datos = rowToArray(line);
-		
-		unsigned int i = 0;
-
-		while (i < ids.size()){
-			if (datos[1] == ids[i] ){
-				break;
-				i++;
-			}
-		}
-		if (i == ids.size()){
-			ids.push_back(datos[1]);
-		}
-
-		unsigned int j = 0;
-
-		while(j < ids.size()){
-			if (datos[3] == ids[j]){
-				break;
-				j++;
-			}
-		}
-		if (j == ids.size()){
-			ids.push_back(datos[3]);
-		}
-	}
-	partidos.clear();
-	partidos.seekg(0,ios::beg);
-	vector<vector<int> > victDerrt(ids.size(),vector<int>(2, 0));
-	vector<vector<int> > enfrentamientos(ids.size(),vector<int>(ids.size(), 0));
-
-	lines = 0;
-	for (;getline(partidos,line);lines++) {
-		vector<string> datos = rowToArray(line);
-		
-		int i = busquedaIndice(datos[1], ids);
-		int j = busquedaIndice(datos[3], ids);
-
-		victDerrt[i][0]++; //victorias
-		victDerrt[j][1]++; //derrotas
-		enfrentamientos[i][j]++;
-	}
-	return victDerrt; 
-}
 
 int main () {
 	fstream partidos("./data/atp_matches_2015.dat"); //abro el .dat de los partidos de tenis TODO
@@ -134,7 +77,6 @@ int main () {
 	lines = 0;
 	for (;getline(partidos,line);lines++) {
 		vector<string> datos = rowToArray(line);
-		
 		int i = busquedaIndice(datos[1], ids);
 		int j = busquedaIndice(datos[3], ids);
 
@@ -142,8 +84,13 @@ int main () {
 		victDerrt[j][1]++; //derrotas
 		enfrentamientos[i][j]++;
 	}
-	vector<vector<int> > cmm = CMM(victDerrt);
-       	
+
+	for(unsigned int i = 0;i < ids.size();i++){
+		enfrentamientos[i][i] = victDerrt[i][0] + victDerrt[i][1];
+	}
+
+	vector<vector<int> > cmm = CMM(enfrentamientos);
+        
 	return 0;	
 }
 
