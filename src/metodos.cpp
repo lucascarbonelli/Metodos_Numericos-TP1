1779a8f165f01e1de765cbc2e58cfe363762f515
lucascarbonelli <luc92ac@hotmail.com>
2017-04-13 20:29:33 -0300

    termina test, y no pasa

--- src/metodos.cpp
+++ src/metodos.cpp
@@ -45,7 +45,7 @@ void eliminacionGaussiana(vector<vector<double> > &A, vector<double> &b)
         // Se analiza el caso en el que el pivot "es cero"
         // Se asume que el sistema tiene solucion y es unica
         // Si para toda fila menor que filaDelPivot, la posicion j vale 0, el algoritmo falla estrepitosamente
-        while(filaDelPivot < dimension)
+        while(abs(A[filaDelPivot][j]) > 0 && filaDelPivot < dimension)
         {
             int filaSwap = buscarFilaPivotInferior(A, filaDelPivot+1, j);
             if(filaSwap < dimension)
@@ -90,7 +90,7 @@ void eliminacionGaussiana(vector<vector<double> > &A, vector<double> &b)
 int buscarFilaPivotInferior(const vector<vector<double> > &A, int fila, int col)
 {
     int dimension = A.size();
-    while(fila < dimension)
+    while(fila < dimension && A[fila][col] > 0)
     {
         ++fila;
     }
